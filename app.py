d6c06685b78dc59c20164cc61542fa8442d8fab4
dyorit2004 <dyorit2004@gmail.com>
2025-06-05 15:10:24 +0530

    add initial files

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+
+app = Flask(__name__)
+model = load_model("pneumonia1.h5")
+UPLOAD_FOLDER = os.path.join('static','uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure upload folder exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+def prepare_image(file_path):
+    img = Image.open(file_path).convert("RGB")
+    img = img.resize((255, 255))
+    img_array = np.array(img)
+    img_array = img_array / 255.0
+    img_array = img_array.reshape(1, 255, 255, 3)
+    return img_array
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        if "image" not in request.files:
+            return redirect(request.url)
+        file = request.files["image"]
+        if file.filename == "":
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(file_path)
+
+            image = prepare_image(file_path)
+            prediction = float(model.predict(image)[0][0])
+            label = "Pneumonia" if prediction > 0.5 else "Normal"
+            image_url=url_for('static',filename='uploads/' + filename)
+            return render_template("index.html", prediction=label, image_url=image_url)
+    return render_template("index.html", prediction=None)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ train_model.py
@@ -0,0 +1,69 @@
+import cv2
+from PIL import Image
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import normalize
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+
+img_directory = 'D:/Data_Science/programs(practical)/ML_python/medical_image_processing/pneumonia_dataset/'
+healthy = os.listdir(img_directory + 'NORMAL/')
+pneumonia = os.listdir(img_directory + 'PNEUMONIA/')
+
+dataset = []
+label = []
+
+input_size1 = 255
+
+for img in healthy:
+    if img.endswith('.jpeg'):
+        image = cv2.imread(img_directory + 'NORMAL/' + img)
+        image = Image.fromarray(image, 'RGB')
+        image = image.resize((input_size1, input_size1))
+        dataset.append(np.array(image))
+        label.append(0)
+
+for img in pneumonia:
+    if img.endswith('.jpeg'):
+        image = cv2.imread(img_directory + 'PNEUMONIA/' + img)
+        image = Image.fromarray(image, 'RGB')
+        image = image.resize((input_size1, input_size1))
+        dataset.append(np.array(image))
+        label.append(1)
+
+dataset = np.array(dataset)
+label = np.array(label)
+
+x_train, x_test, y_train, y_test = train_test_split(dataset, label, test_size=0.2, random_state=0)
+
+x_train = normalize(x_train, axis=1)
+x_test = normalize(x_test, axis=1)
+
+model = Sequential()
+model.add(Conv2D(32, (3,3), input_shape=(input_size1, input_size1, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(32, (3,3), kernel_initializer='he_uniform'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3,3), kernel_initializer='he_uniform'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=16, verbose=1, epochs=100, validation_data=(x_test, y_test), shuffle=False)
+
+model.save('pneumonia1.h5')
+
+
